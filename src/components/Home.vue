6e79ad8b83ff5a61032d59168eb292a7a8d5aa04
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2017-11-15 22:19:12 +0100

    Add menu to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,6 +4,13 @@
     class="ui inverted vertical masthead center aligned segment"
     style="background-image: url(public/vegetables_basket.jpg)"
   >
+    <div class="ui container">
+      <div class="ui large secondary orange pointing menu">
+        <a class="active item">Matkoll</a>
+        <a class="item">Om</a>
+      </div>
+    </div>
+
     <div class="ui text container">
       <h1 class="ui inverted header">
         Få koll på maten du äter
@@ -39,6 +46,15 @@ export default {
 .masthead {
   background-size: cover !important;
   min-height: 640px;
+}
+
+.masthead .menu {
+  border-bottom: 0 !important;
+}
+
+.masthead .menu .item {
+  border-color: #f99157;
+  color: #f99157;
 }
 
 .masthead h1.ui.header {
