61f9b7f53798f24ffce5eacfef3f284fec4679fa
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2017-11-15 22:19:34 +0100

    Use single quotes, not double

--- src/components/charts/nutrient-bar-chart.js
+++ src/components/charts/nutrient-bar-chart.js
@@ -6,16 +6,16 @@ export default Pie.extend({
   mounted () {
     this.renderChart({
       labels: this.nutrients.map(n => n.name),
-        datasets: [
-          {
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            borderWidth: 2,
-            hoverBackgroundColor: "rgba(255,99,132,0.4)",
-            hoverBorderColor: "rgba(255,99,132,1)",
-            data: this.nutrients.map(n => n.value),
-          }
-        ]
+      datasets: [
+        {
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 2,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: this.nutrients.map(n => n.value),
+        }
+      ]
     }, {
       title: { display: false },
       legend: { display: false }
