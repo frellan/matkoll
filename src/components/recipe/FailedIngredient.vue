e434a3002b5c6b0d47fc33591f2109693c19170b
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2017-12-12 11:17:32 +0100

    Add FailedIngredient component

---
+++ src/components/recipe/FailedIngredient.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="ui red inverted segment">
+  {{ failedIngredient }}
+</div>
+</template>
+
+<script>
+export default {
+  name: 'failed-ingredient',
+  props: ['failedIngredient']
+}
+</script>
+
+<style scoped>
+.ui.segment {
+  padding-top: 0.7em;
+  padding-bottom: 0.7em;
+}
+.comment {
+  font-size: 0.8em;
+  color: rgba(0,0,0,.4);
+}
+</style>
--- src/components/recipe/IngredientList.vue
+++ src/components/recipe/IngredientList.vue
@@ -2,40 +2,47 @@
 <div>
   <div v-if="sections">
     <div class="section" v-for="section in sections">
-      <h2 class="centered">
-        {{ section.title.titleize() }}
-      </h2>
-      <div class="ui one stackable cards">
-        <ingredient
-          v-for="ingredient in section.ingredients"
-          :key="ingredient.original"
-          :ingredient="ingredient"
-        ></ingredient>
-    </div>
-    </div>
-  </div>
-  <div class="section" v-if="ingredients">
-    <div class="ui one stackable cards">
+      <h3 class="centered">{{ section.title.titleize() }}</h3>
       <ingredient
-        v-for="ingredient in ingredients"
+        v-for="ingredient in section.ingredients"
         :key="ingredient.original"
         :ingredient="ingredient"
       ></ingredient>
+      <failed-ingredient
+        v-for="failed in section.failedIngredients"
+        :key="failed"
+        :failedIngredient="failed"
+      ></failed-ingredient>
     </div>
+  </div>
+  <div class="section" v-if="ingredients">
+    <ingredient
+      v-for="ingredient in ingredients"
+      :key="ingredient.original"
+      :ingredient="ingredient"
+    ></ingredient>
+    <failed-ingredient
+      v-for="failed in failedIngredients"
+      :key="failed"
+      :failedIngredient="failed"
+    ></failed-ingredient>
   </div>
 </div>
 </template>
 
 <script>
 import Ingredient from './Ingredient'
+import FailedIngredient from './FailedIngredient'
 export default {
   name: 'ingredient-list',
   props: [
     'sections',
-    'ingredients'
+    'ingredients',
+    'failedIngredients'
   ],
   components: {
-    Ingredient
+    Ingredient,
+    FailedIngredient
   }
 }
 </script>
